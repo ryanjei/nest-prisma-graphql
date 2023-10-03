8cde0ce5df53c18eaa2e22fb055bbe39d54933f2
ryanjei <ryanjei.degoma@japan-systems.co.jp>
2023-10-04 00:57:01 +0900

    Read, Update, Deleteを追加

--- src/modules/post/post.resolver.ts
+++ src/modules/post/post.resolver.ts
@@ -11,11 +11,30 @@ export class PostResolver {
     return this.postService.findOne(id);
   }
 
+  @Query(() => [Post], { nullable: true })
+  async posts(): Promise<Post[] | null> {
+    return this.postService.findMany();
+  }
+
   @Mutation(() => Post, { nullable: true })
   async createPost(
     @Args('title') title: string,
     @Args('content') content: string | null,
   ): Promise<Post> {
     return this.postService.create(title, content);
+  }
+
+  @Mutation(() => Post, { nullable: true })
+  async updatePost(
+    @Args('id') id: number,
+    @Args('title') title: string,
+    @Args('content') content: string | null,
+  ): Promise<Post> {
+    return this.postService.update(id, title, content);
+  }
+
+  @Mutation(() => Post, { nullable: true })
+  async deletePost(@Args('id') id: number): Promise<Post> {
+    return this.postService.delete(id);
   }
 }
--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -12,11 +12,35 @@ export class PostService {
     });
   }
 
+  async findMany(): Promise<Post[] | null> {
+    return this.prisma.post.findMany();
+  }
+
   async create(title: string, content: string): Promise<Post> {
     return this.prisma.post.create({
       data: {
         title,
         content,
+      },
+    });
+  }
+
+  async update(id: number, title: string, content: string): Promise<Post> {
+    return this.prisma.post.update({
+      where: {
+        id,
+      },
+      data: {
+        title,
+        content,
+      },
+    });
+  }
+
+  async delete(id: number): Promise<Post> {
+    return this.prisma.post.delete({
+      where: {
+        id,
       },
     });
   }
