829561c40d4abb96635c2c1cd309c33b661981d2
ryanjei <ryanjei.degoma@japan-systems.co.jp>
2023-10-15 14:16:41 +0900

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000, () => {
+
+  // CORSを有効にし、必要に応じて設定します
+  app.enableCors({
+    origin: 'http://localhost:3000', // フロントエンドの実際のオリジンに置き換えてください
+    methods: 'GET,POST',
+    credentials: true, // リクエストにクッキーを含める必要がある場合に設定
+  });
+
+  await app.listen(3001, () => {
     console.log(`
-    🚀 Server ready at: http://localhost:3000/graphql
+    🚀 Server ready at: http://localhost:3001/graphql
     `);
   });
 }
